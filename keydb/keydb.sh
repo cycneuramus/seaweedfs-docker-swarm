d53201a5bef24fb82d064e794ae853455f29a02b
a <a@local>
2023-01-04 22:55:04 +0100

    KeyDB: switch to ring topology and increase resilience

--- keydb/keydb.sh
+++ keydb/keydb.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-nodes="replicaof keydb_node1 6379
-replicaof keydb_node2 6379
-replicaof keydb_node3 6379
-replicaof keydb_node4 6379"
-
-nodes=$(echo "$nodes" | grep -v "$HOST")
+if [ "$HOST" = "node1" ]; then
+	replica=node2
+elif [ "$HOST" = "node2" ]; then
+	replica=node3
+elif [ "$HOST" = "node3" ]; then
+	replica=node4
+elif [ "$HOST" = "node4" ]; then
+	replica=node1
+fi
 
 cat > /etc/keydb.conf <<- EOF
-	appendonly yes
-	active-replica yes
-	multi-master yes
-	repl-backlog-size 1000mb
-	repl-timeout 600
-	client-output-buffer-limit normal 0 0 0
-	client-output-buffer-limit pubsub 0 0 0
-	client-output-buffer-limit replica 0 0 0
-	$nodes
+	    appendonly yes
+	    active-replica yes
+	    multi-master yes
+	    repl-backlog-size 100mb
+	    repl-timeout 300
+	    allow-write-during-load yes
+	    client-output-buffer-limit normal 0 0 0
+	    client-output-buffer-limit pubsub 0 0 0
+	    client-output-buffer-limit replica 0 0 0
+	    replicaof keydb_$replica 6379
 EOF
-
-keydb-server /etc/keydb.conf
