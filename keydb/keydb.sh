5c4e2a497491fb4f72aa2f5e3f2d5fc99ffc4cb2
a <a@local>
2023-01-04 22:59:45 +0100

    Fix accidental deletion

--- keydb/keydb.sh
+++ keydb/keydb.sh
@@ -11,14 +11,16 @@ elif [ "$HOST" = "node4" ]; then
 fi
 
 cat > /etc/keydb.conf <<- EOF
-	    appendonly yes
-	    active-replica yes
-	    multi-master yes
-	    repl-backlog-size 100mb
-	    repl-timeout 300
-	    allow-write-during-load yes
-	    client-output-buffer-limit normal 0 0 0
-	    client-output-buffer-limit pubsub 0 0 0
-	    client-output-buffer-limit replica 0 0 0
-	    replicaof keydb_$replica 6379
+	appendonly yes
+	active-replica yes
+	multi-master yes
+	repl-backlog-size 100mb
+	repl-timeout 300
+	allow-write-during-load yes
+	client-output-buffer-limit normal 0 0 0
+	client-output-buffer-limit pubsub 0 0 0
+	client-output-buffer-limit replica 0 0 0
+	replicaof keydb_$replica 6379
 EOF
+
+keydb-server /etc/keydb.conf
